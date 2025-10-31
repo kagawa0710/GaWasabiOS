68cbd50c7b13bba975676acf0119bef4f6b9109c
kagawa0710 <yuuya081@icloud.com>
2025-11-01 00:15:25 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+#![no_std]
+#![no_main]
+#![feature(offset_of)]
+
+use core::mem::offset_of;
+use core::mem::size_of;
+use core::panic::PanicInfo;
+use core::ptr::null_mut;
+use core::slice;
+
+type EfiVoid = u8;
+type EfiHandle = u64;
+type Result<T> = core::result::Result<T, &'static str>;
+
+#[repr(C)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+struct EfiGuid {
+    data0: u32,
+    data1: u16,
+    data2: u16,
+    data3: [u8; 8],
+}
+
+const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EfiGuid = EfiGuid {
+    data0: 0x9042a9de,
+    data1: 0x23dc,
+    data2: 0x4a38,
+    data3: [0x96, 0xfb, 0x7a, 0xde, 0xd0, 0x80, 0x51, 0x6a],
+};
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+#[must_use]
+#[repr(u64)]
+enum EfiStatus {
+    Success = 0,
+}
+
+#[repr(C)]
+struct EfiBootServicesTable {
+    _reserved0: [u64; 40],
+    locate_protocol: extern "win64" fn(
+        protocol: *const EfiGuid,
+        registration: *mut EfiVoid,
+        interface: *mut *mut EfiVoid,
+    ) -> EfiStatus,
+}
+const _: () = assert!(offset_of!(EfiBootServicesTable, locate_protocol) == 320);
+
+#[repr(C)]
+struct EfiSystemTable {
+    _reserved0: [u64; 12],
+    pub boot_services: &'static EfiBootServicesTable,
+}
+const _: () =assert!(offset_of!(EfiSystemTable, boot_services) == 96);
+
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocolPixelInfo {
+    pub version: u32,
+    pub horizontal_resolution: u32,
+    pub vertical_resolution: u32,
+    _padding0:[u32; 5],
+    pub pixels_per_scan_line: u32,
+}
+const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
+
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocolMode<'a> {
+    pub max_mode: u32,
+    pub mode: u32,
+    pub info: &'a EfiGraphicsOutputProtocolPixelInfo,
+    pub size_of_info: u32,
+    pub frame_buffer_base: usize,
+    pub frame_buffer_size: usize,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocol<'a> {
+    reserved: [u64; 3],
+    pub mode: &'a EfiGraphicsOutputProtocolMode<'a>,
+}
+fn locate_graphic_protocol<'a>(
+    efi_system_table: &'a EfiSystemTable,
+) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
+    let mut efi_graphics_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
+    let status = (efi_system_table.boot_services.locate_protocol)(
+        &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
+        null_mut::<EfiVoid>(),
+        &mut efi_graphics_output_protocol as *mut *mut EfiGraphicsOutputProtocol as *mut *mut EfiVoid,
+    );
+    if status != EfiStatus::Success {
+        return Err("Failed to locate graphics outptut protocol")
+    }
+    Ok(unsafe { &*efi_graphics_output_protocol })
+}
+
+#[no_mangle]
+// The entry point for the EFI application(仕様でEFIアプリケーションのエントリポイントはefi_mainとなっている)
+fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
+    // locate_graphics(...)を使ってUEFIのGraphics Output Protocolを取得すし，画面への書き込み情報を得る．
+    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+
+    // VRAM(映像メモリ)へのポインタ作成
+    // 生のポインタをRustの可変スライスに変換．
+    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
+    let vram = unsafe {
+        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
+    };
+
+    // スライスをループして白色で埋める
+    for e in vram {
+        *e = 0xffffff;
+    }
+
+    // println!("Hello, world!");
+
+    // 画面を保つために無限ループ
+    loop {}
+}
+
+// panic!()が呼ばれたときの処理
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
